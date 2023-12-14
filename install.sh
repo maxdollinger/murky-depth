0708a31c1348ad3df7c7f9be2ddaea0804e7317d
Maximilian Dollinger <70093586+maxdollinger@users.noreply.github.com>
2023-12-14 15:37:58 +0100

    Update install.sh macos

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-#! /usr/bin/bash
+#! /bin/sh
 
 delim="----------------------------------"
 
 read -p "Install packages? [y/n] " sin
 if [ $sin == "y" ]; then
-    pkgs="zsh kitty lf lazygit fzf tmux neovim jq wl-clipboard otf-firamono-nerd"
+    pkgs="kitty lf lazygit fzf tmux neovim jq"
     for i in $pkgs; do
         [[ -n "$(command -v $i)" ]] && continue 
         
-        sudo pacman -S $i
+        sudo brew install $i
     done
 fi
 echo $delim
@@ -22,16 +22,6 @@ if [ $sin == "y" ]; then
     cp -a  ~/murky-depth/.zshrc ~/
     rm -rf ~/murky-depth
     echo "source ~/.zsh/settings.zsh" >> ~/.zshrc
-fi
-echo $delim
-
-read -p "Download EOS Wallpapers? [y/n] " sin
-if [ $sin == "y" ]; then
-    curl -L https://github.com/EndeavourOS-Community-Editions/Community-wallpapers/archive/refs/heads/main.zip -o ~/Downloads/ebg.zip
-    unzip -j ~/Downloads/ebg.zip -d ~/.local/share/backgrounds/
-    rm -rf ~/Downloads/ebg.zip
-    gsettings set org.gnome.desktop.background picture-uri-dark "'file://$HOME/.local/share/backgrounds/EOS-SPACE-4K.png'"
-    gsettings set org.gnome.desktop.background picture-uri "'file://$HOME/.local/share/backgrounds/EOS-SPACE-4K.png'"
 fi
 echo $delim
 
@@ -80,7 +70,7 @@ fi
 if [ -z "$(command -v nvm)" ]; then
     read -p "Install node and nvm? [y/n] " sin
     if [ $sin == "y" ]; then
-        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | zsh
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
         source ~/.zshrc 2> /dev/null
         nvm install node
         read -p "Install Typescript globaly? [y/n] " sin
@@ -100,45 +90,3 @@ if [ $sin == "y" ]; then
     go version
 fi
 echo $delim
-
-read -p "Install gnome extentions? [y/n] " sin
-if [ $sin == "y" ]; then
-    ext="run-or-raise@edvard.cz Vitals@CoreCoding.com gnome-clipboard@b00f.github.io blur-my-shell@aunetx"
-
-    for i in $ext; do
-        vs=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=${i}" | jq '.extensions[0].shell_version_map | map(.version) | max')
-        name="$(echo $i | sed 's/@//')"
-        url="https://extensions.gnome.org/extension-data/${name}.v${vs}.shell-extension.zip"
-        curl $url --output "${i}.zip"
-        gnome-extensions install --force "${i}.zip"
-        gnome-extensions enable ${i}
-        rm "${i}.zip"
-    done
-fi
-echo $delim
-
-read -p "Apply gnome settings? [y/n] " sin
-echo $delim
-if [ $sin == "y" ]; then
-    gsettings set org.gnome.mutter dynamic-workspaces false
-    gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
-    ks="1 2 3 4 5 6"
-    for i in $ks; do
-        gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-$i" "['<Alt>$i']"
-        gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-$i" "['<Alt><Shift>$i']"
-    done
-
-    gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>q']"
-    gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Alt>m']"
-
-    gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Alt>h']"
-    gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Alt>l']"
-
-    gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
-
-    gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-    
-    gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
-    gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-    gsettings set org.gnome.desktop.peripherals.touchpad speed 0.5
-fi
