5772d465f5266a3728c1ecea3633dec32f8cbf6a
Maximilian Dollinger <max@dollinger.dev>
2024-03-03 10:07:36 +0100

    1.3

--- .zsh/settings.zsh
+++ .zsh/settings.zsh
@@ -19,25 +19,16 @@ alias lf='lfcd'
 
 trash() {
     trash_dir="${HOME}/.trash"
-    # Create .trash directory if it doesn't exist
+
    if [ ! -d "$trash_dir" ]; then
         mkdir "$trash_dir"
-    fi
-    # Get the current timestamp
-    timestamp=$(date +%s)
-    # Loop through each file argument
-    for arg in "$@"; do
-        # Check if the arg exists
-        if [ -e "$arg" ]; then
-            # Copy the arg to .Tras
-            name=$(basename "$arg")
-            cp -a "$arg" "${trash_dir}/${timestamp}_${name}" && rm -rf "$arg"
-            # Remove the original arg
-            echo "Moved $arg to Trash"
-        else
-            echo "$arg does not exist"
-        fi
-    done
+   fi
+
+   timestamp=$(date +%s)
+   for arg in "$@"; do
+       name=$(basename "$arg")
+       mv -fv "$arg" "${trash_dir}/${timestamp}_${name}"
+   done
 }
 
 empty () {
